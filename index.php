a807f3a2559a64dd9198fc0d484b24575ed17de1
Dean5w6 <deanjoefreyii@gmail.com>
2025-08-19 10:37:07 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Music App</title>
+</head>
+<body>
+    <h1>Testing my setup!</h1>
+    <p>
+        <?php
+            echo "If you see this message, PHP is working correctly!";
+            echo "<br>";
+            echo "The current date is: " . date("m-d-Y");
+        ?>
+    </p>
+</body>
+</html>
